965f67194a7d74adc9bded3523afd941f91b13ae
DevJake99 <jacobg4@uci.edu>
2023-10-31 15:38:45 -0700

    adding boiler plate structure from similar project

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Helper function to ensure only logged in users can access site
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
